99b7ce881cac713da3628331cb29888ddc1afb76
Kasumi Akiyama★ <shellyakiyama@gmail.com>
2024-08-03 23:05:00 -0600

    Updated server side

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,189 +1,77 @@
-<<<<<<< HEAD
-const { Profile } = require('../models');
-=======
-const { User, Thought } = require('../models');
->>>>>>> origin/main
+const { ProvidedRequiredArgumentsRule } = require('graphql');
+const { User, Story } = require('../models');
 const { signToken, AuthenticationError } = require('../utils/auth');
 
 const resolvers = {
   Query: {
-<<<<<<< HEAD
-    profiles: async () => {
-      return Profile.find();
-    },
-
-    profile: async (parent, { profileId }) => {
-      return Profile.findOne({ _id: profileId });
-    },
-    // By adding context to our query, we can retrieve the logged in user without specifically searching for them
-    me: async (parent, args, context) => {
-      if (context.user) {
-        return Profile.findOne({ _id: context.user._id });
-=======
-    users: async () => {
-      return User.find().populate('thoughts');
-    },
-    user: async (parent, { username }) => {
-      return User.findOne({ username }).populate('thoughts');
-    },
-    thoughts: async (parent, { username }) => {
-      const params = username ? { username } : {};
-      return Thought.find(params).sort({ createdAt: -1 });
-    },
-    thought: async (parent, { thoughtId }) => {
-      return Thought.findOne({ _id: thoughtId });
-    },
-    me: async (parent, args, context) => {
-      if (context.user) {
-        return User.findOne({ _id: context.user._id }).populate('thoughts');
->>>>>>> origin/main
+    me: async (_, args, context) =>  {
+      if(!context?.user) {
+        throw AuthenticationError;
+      } else {
+        const foundUser = await User.findOne({_id: context.user._id }).populate("current_story")
+        ;
+    
+        if (!foundUser) {
+          // return res.status(400).json({ message: 'Cannot find a user with this id!' });
+          throw AuthenticationError;
+        }
+    
+        // res.json(foundUser);
+        return (foundUser);
       }
-      throw AuthenticationError;
     },
   },
 
   Mutation: {
-<<<<<<< HEAD
-    addProfile: async (parent, { name, email, password }) => {
-      const profile = await Profile.create({ name, email, password });
-      const token = signToken(profile);
-
-      return { token, profile };
-    },
-    login: async (parent, { email, password }) => {
-      const profile = await Profile.findOne({ email });
-
-      if (!profile) {
+    goNextStory: async(_, args, context) => {
+      if(!context?.user) {
         throw AuthenticationError;
-      }
+      } else {
 
-      const correctPw = await profile.isCorrectPassword(password);
-=======
-    addUser: async (parent, { username, email, password }) => {
-      const user = await User.create({ username, email, password });
-      const token = signToken(user);
-      return { token, user };
-    },
-    login: async (parent, { email, password }) => {
-      const user = await User.findOne({ email });
+        // find story based of received nextStoryId
+        const foundStory = await Story.findOne({
+          _id: args.nextStoryId
+        })
+
+        if(!foundStory) {
+          throw AuthenticationError
+        }
+
+        // update user to next story id
+        await User.findOneAndUpdate(
+          {_id: context.user._id},
+          {current_story: foundStory._id}
+        )
+
+
+        return foundStory;
+
+      }
+    }, 
+    login: async (parent, { username, password }) => {
+      const user = await User.findOne({ username });
 
       if (!user) {
         throw AuthenticationError;
       }
 
       const correctPw = await user.isCorrectPassword(password);
->>>>>>> origin/main
 
       if (!correctPw) {
         throw AuthenticationError;
       }
 
-<<<<<<< HEAD
-      const token = signToken(profile);
-      return { token, profile };
-    },
-
-    // Add a third argument to the resolver to access data in our `context`
-    addSkill: async (parent, { profileId, skill }, context) => {
-      // If context has a `user` property, that means the user executing this mutation has a valid JWT and is logged in
-      if (context.user) {
-        return Profile.findOneAndUpdate(
-          { _id: profileId },
-          {
-            $addToSet: { skills: skill },
-=======
       const token = signToken(user);
-
       return { token, user };
     },
-    addThought: async (parent, { thoughtText }, context) => {
-      if (context.user) {
-        const thought = await Thought.create({
-          thoughtText,
-          thoughtAuthor: context.user.username,
-        });
+    // for creating user
+    signup: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
 
-        await User.findOneAndUpdate(
-          { _id: context.user._id },
-          { $addToSet: { thoughts: thought._id } }
-        );
-
-        return thought;
-      }
-      throw AuthenticationError;
-      ('You need to be logged in!');
+      const token = signToken(user);
+      return { token, user };
     },
-    addComment: async (parent, { thoughtId, commentText }, context) => {
-      if (context.user) {
-        return Thought.findOneAndUpdate(
-          { _id: thoughtId },
-          {
-            $addToSet: {
-              comments: { commentText, commentAuthor: context.user.username },
-            },
->>>>>>> origin/main
-          },
-          {
-            new: true,
-            runValidators: true,
-          }
-        );
-      }
-<<<<<<< HEAD
-      // If user attempts to execute this mutation and isn't logged in, throw an error
-      throw AuthenticationError;
-    },
-    // Set up mutation so a logged in user can only remove their profile and no one else's
-    removeProfile: async (parent, args, context) => {
-      if (context.user) {
-        return Profile.findOneAndDelete({ _id: context.user._id });
-      }
-      throw AuthenticationError;
-    },
-    // Make it so a logged in user can only remove a skill from their own profile
-    removeSkill: async (parent, { skill }, context) => {
-      if (context.user) {
-        return Profile.findOneAndUpdate(
-          { _id: context.user._id },
-          { $pull: { skills: skill } },
-=======
-      throw AuthenticationError;
-    },
-    removeThought: async (parent, { thoughtId }, context) => {
-      if (context.user) {
-        const thought = await Thought.findOneAndDelete({
-          _id: thoughtId,
-          thoughtAuthor: context.user.username,
-        });
-
-        await User.findOneAndUpdate(
-          { _id: context.user._id },
-          { $pull: { thoughts: thought._id } }
-        );
-
-        return thought;
-      }
-      throw AuthenticationError;
-    },
-    removeComment: async (parent, { thoughtId, commentId }, context) => {
-      if (context.user) {
-        return Thought.findOneAndUpdate(
-          { _id: thoughtId },
-          {
-            $pull: {
-              comments: {
-                _id: commentId,
-                commentAuthor: context.user.username,
-              },
-            },
-          },
->>>>>>> origin/main
-          { new: true }
-        );
-      }
-      throw AuthenticationError;
-    },
-  },
+  }
 };
 
 module.exports = resolvers;
