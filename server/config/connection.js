d2c2a66b5788c74449622e99a542ba9218f322b9
Yvonne <yowantings@gmail.com>
2024-08-05 19:59:59 -0600

    added code to forked repo

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,9 +1,5 @@
 const mongoose = require('mongoose');
 
-<<<<<<< HEAD
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/tech-friends');
-=======
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/programming-thoughts');
->>>>>>> origin/main
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/mern-shopping');
 
 module.exports = mongoose.connection;
